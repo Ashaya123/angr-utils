b8591efebcd8304a31a56f2f557a5ed8b9888f32
Ashaya123 <ashaya.vanasuma@gmail.com>
2019-08-19 16:26:46 -0400

    Update plot_cfg_path_example.py

--- examples/plot_cfg_path/plot_cfg_path_example.py
+++ examples/plot_cfg_path/plot_cfg_path_example.py
@@ -42,7 +42,7 @@ def analyze(b, addr, name=None):
             c += 1
 
 if __name__ == "__main__":
-    proj = angr.Project("../samples/ais3_crackme", load_options={'auto_load_libs':False})
+    proj = angr.Project("/Users/ashaya/Downloads/simple", load_options={'auto_load_libs':False})
     main = proj.loader.main_object.get_symbol("main")
     analyze(proj, main.rebased_addr, "ais3")
 
